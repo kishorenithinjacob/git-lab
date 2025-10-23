0e5bed47473d5abcdcbfc7453f46e10db172236b
kishorenithinjacob <kishorenithinjacob1126@gmail.com>
2025-10-23 10:56:31 +0530

    Feature: upgrade greet() to v2 and append feature note

--- src/hello.py
+++ src/hello.py
@@ -1,5 +1,6 @@
 def greet(name: str) -> str:
-    return f"Hello, Git Learner! (v1)"
+    # friendlier message + emoji (v2)
+    return f"Hello, Git Learner! Welcome to the feature branch 🚀 (v2)"
 
 if __name__ == "__main__":
     print(greet("Git Learner"))
